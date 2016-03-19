48cddfd7984366cf32673e863fe0b5b0d28edcef
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2016-03-19 18:30:01 +0200

    casting functions
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- leason_1.py
+++ leason_1.py
@@ -6,7 +6,20 @@ import unicodecsv as csvReader
 def readCsvFiles (fileName):
     with open(fileName, 'rb') as f:
         reader = csvReader.DictReader(f)
-        elements = list(reader)
-        return elements
+        return list(reader)
 
-print (len(readCsvFiles('Data/enrollments.csv')))
+def possible_int(field):
+    if (field == ''):
+        return None
+    else:
+        try:
+            return int(field)
+        except:
+            return None
+
+def possible_bool(field):
+    if(field == 'true' or field == 'True'):
+        return True
+    return False
+print (readCsvFiles('C://Users//AhmedA//Desktop//Data//daily_engagement.csv')[0])
+#print(possible_int('1501'))
